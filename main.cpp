c94481b3db0ef9a235947052d598f59af6fa6597
stardust <open-src@qq.com>
2025-12-31 10:42:33 +0800

    modified main()

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main()
     // alias for static function Registrar::singleton
     auto system = &Registrar::singleton;
 
-    system().initialize();
+    Registrar::singleton().initialize();
 
     // Simulate some students erolls in some courses
     system().studentEnrollsInCourse("S001", "CS101");
